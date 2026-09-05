e83d30b9f488aa3c34ab6dbcd68477c88fa10aa4
vishalpraja777 <vishalprajapathi7@gmail.com>
2026-09-05 12:50:53 +0530

    Build fix

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -30,6 +30,7 @@ export default function App() {
             onEnablePersonalSearch={() => setPersonalSearch(true)}
             onDisablePersonalSearch={() => setPersonalSearch(false)}
           />
+        </div>
       </TooltipProvider>
     </MotionConfig>
   );
